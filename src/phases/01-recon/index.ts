912991c9a6cd5023ac84c02618f26c24ea952ee3
Chmod351 <yamil.t.351@proton.me>
2026-04-02 17:48:17 -0300

    uso de streaming

--- src/phases/01-recon/index.ts
+++ src/phases/01-recon/index.ts
@@ -1,8 +1,7 @@
-import {getAllSubdomains} from "./subdomainFinder.ts";
+import { logger } from "../../shared/errorLogger.ts";
+import {streamAllSubdomains} from "./subdomainFinder.ts";
 
-export async function reconPhase(target:string){
-  console.log("entrando en recon...");
-const subdomains= await getAllSubdomains(target);
-console.log("--FASE 1 : TERMINADA");
-return subdomains;
+export function reconPhase(target:string){
+  logger.debug("recon iniciando", `${target}`)
+  return streamAllSubdomains(target); 
 }
