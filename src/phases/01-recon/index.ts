ab6ba7190ff061ebc51ee733d88f54dda01d8963
Chmod351 <yamil.t.351@proton.me>
2026-04-03 08:53:57 -0300

    add logger

--- src/phases/01-recon/index.ts
+++ src/phases/01-recon/index.ts
@@ -2,6 +2,7 @@ import { logger } from "../../shared/errorLogger.ts";
 import {streamAllSubdomains} from "./subdomainFinder.ts";
 
 export function reconPhase(target:string){
-  logger.debug("recon iniciando", `${target}`)
-  return streamAllSubdomains(target); 
+  logger.debug("PHASE-01",`Iniciando reconocimiento de subdominios para ${target}`)
+  const domains= streamAllSubdomains(target); 
+  return domains
 }
