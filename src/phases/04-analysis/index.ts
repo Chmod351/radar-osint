bd4f519fbea391f8c057f53d17bcd6f7fdc4c5c1
Chmod351 <yamil.t.351@proton.me>
2026-04-04 10:37:10 -0300

    eslint y elimine loggers

--- src/phases/04-analysis/index.ts
+++ src/phases/04-analysis/index.ts
@@ -2,7 +2,7 @@ import { logger } from "../../shared/errorLogger.ts";
 import { httpPhaseAndVulnerabilityPhaseMerger } from "./vulnScanner.ts";
 
 export async function analysisPhase(webAssets: any[]) {
-logger.info("PHASE 4:", "Fase 4 iniciada...")
+logger.info("PHASE 4:", "Fase 4 iniciada...");
   // USAMOS MAP + PROMISE.ALL PARA PROCESAR TODO EL ARRAY
   const finalReport = await Promise.all(
     webAssets.map(async (asset) => {
@@ -12,6 +12,6 @@ logger.info("PHASE 4:", "Fase 4 iniciada...")
   );
 
  
-  logger.info("PHASE 4:",  "Fase 4 terminada ")
+  logger.info("PHASE 4:",  "Fase 4 terminada ");
   return finalReport; 
 }
